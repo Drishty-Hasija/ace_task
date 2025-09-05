8532f29e75358dc4f3c231c1f85f9c45ecd0b919
Drishty-Hasija <drishty129@gmail.com>
2025-09-05 16:42:58 +0530

    commit changes

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatelessWidget { 
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
@@ -25,80 +25,109 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent.shade100,
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Container(
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
+      backgroundColor: Color(0xFF001D3D),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          SizedBox(height: 80),
+          Text(
+            "Ace Task",
+            style: TextStyle(
+              fontFamily: "Roboto",
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
               color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 10)],
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  "Login",
-                  style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blueAccent),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12)),
-                    labelText: "Email",
-                  ),
-                ),
-                SizedBox(height: 15),
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12)),
-                    labelText: "Password",
-                  ),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueAccent,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12)),
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 12),
-                  ),
-                  onPressed: () {
-                    String email = emailController.text;
-                    String password = passwordController.text;
-
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text("Logged in as $email")),
-                    );
-                  },
-                  child: Text("Login", style: TextStyle(fontSize: 18)),
-                ),
-                SizedBox(height: 15),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => SignupPage()));
-                  },
-                  child: Text("Want to create an account? Sign Up"),
-                )
-              ],
             ),
           ),
-        ),
+          SizedBox(height: 80),
+          Image.network(
+            "https://cdn-icons-png.flaticon.com/512/3062/3062634.png",
+            height: 190,
+          ),
+          SizedBox(height: 70),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 10)],
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Login",
+                    style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                       color: Color(0xDA001F41)),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      labelText: "Email",
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.pressed)) {
+                            return Color(0XFF0077b6);
+                          }
+                          return Color(0xDA001F41);
+                        },
+                      ),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                      ),
+                      padding: MaterialStateProperty.all(
+                        EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                      ),
+                    ),
+                    onPressed: () {
+                      String email = emailController.text;
+
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Logged in as $email in Ace Task application")),
+                      );
+                    },
+                    child: Text("Login", style: TextStyle(fontSize: 18, color: Colors.white),),
+                  ),
+                  SizedBox(height: 15),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SignupPage()));
+                    },
+                    child: Text("Don't have an account? Sign Up" , style: TextStyle( color: Color(0xDA001F41))),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -114,7 +143,7 @@ class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pinkAccent.shade100,
+      backgroundColor: Color(0xff301d6b),
       body: Center(
         child: Padding(
           padding: EdgeInsets.all(20),
@@ -133,9 +162,14 @@ class SignupPage extends StatelessWidget {
                   style: TextStyle(
                       fontSize: 28,
                       fontWeight: FontWeight.bold,
-                      color: Colors.pinkAccent),
+                      color: Color(0xff301d6b)),
                 ),
-                SizedBox(height: 20),
+                SizedBox(height: 10),
+                Image.network(
+                  "https://lexworkplace.com/wp-content/uploads/2022/02/document-management-legal-department-1536x1078.png",
+                  height: 170,
+                ),
+                SizedBox(height: 30),
                 TextField(
                   controller: nameController,
                   decoration: InputDecoration(
@@ -169,20 +203,19 @@ class SignupPage extends StatelessWidget {
                 SizedBox(height: 20),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.pinkAccent,
+                    backgroundColor: Color(0xdd301d6b),
                     shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12)),
+                        borderRadius: BorderRadius.circular(15)),
                     padding: EdgeInsets.symmetric(horizontal: 40, vertical: 12),
                   ),
                   onPressed: () {
                     String name = nameController.text;
-                    String email = emailController.text;
 
                     ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text("Account created for $name")),
+                      SnackBar(content: Text("Account created for $name in Ace Task application")),
                     );
                   },
-                  child: Text("Sign Up", style: TextStyle(fontSize: 18)),
+                  child: Text("Sign Up", style: TextStyle(fontSize: 18, color: Color(0XFFFFFFFF))),
                 ),
                 SizedBox(height: 15),
                 TextButton(
